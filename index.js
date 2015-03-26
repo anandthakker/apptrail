4bcd5f7cbe419c135394e7fa70ab42f639bbef6a
Anand Thakker <vestibule@anandthakker.net>
2015-03-26 00:21:50 -0400

    wip: render elevation profile

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('mapbox.js'); // attaches to window.L
 
+var createElement = require('virtual-dom/create-element')
 var chooseRoute = require('./lib/choose-route.js');
 var fetchElevation = require('./lib/fetch-elevation.js');
+var profile = require('./lib/profile.js');
 
 require('./app.css');
 
@@ -13,7 +15,18 @@ var map = L.mapbox.map('map', 'anandthakker.99416d58')
 var centerline = require('./data/centerline-merged.json');
 var trailLayer = L.mapbox.featureLayer(centerline).addTo(map);
 
-chooseRoute(map, centerline, locateShelters)
+chooseRoute(map, centerline, routeChanged)
+
+function routeChanged (route) {
+  fetchElevation(route, function(err, elevation) {
+    console.log(err, elevation)
+    if(err) console.error(err)
+    var chart = profile(route, elevation)
+    var el = document.getElementById('profile')
+    el.appendChild(createElement(chart))
+  })
+}
+
 
 function locateShelters (route) {
   var waypoints = require('./lib/waypoints.js')
@@ -21,6 +34,7 @@ function locateShelters (route) {
   var data = waypoints(route, shelters.features)
 
   console.log(data)
+  document.getElementById('profile').innerHTML = JSON.stringify(data)
 
   L.mapbox.featureLayer(data)
   .eachLayer(function (marker) {
